a388912462762daddb1801306cd3e7c5bf1730cb
cnayak <emailchiragnayak@gmail.com>
2020-01-09 13:34:16 +0530

    py wo

--- pyForEverybody/camelCaseCounter.py
+++ pyForEverybody/camelCaseCounter.py
@@ -11,10 +11,10 @@ def camelcaseCount(val):
 	currIndex=0
 	count=1
 	while True:
-		if myList[currIndex].isupper()
+		if myList[currIndex].isupper() :
 			count=count+1
 		currIndex=currIndex+1
-		if currIndex > len(val)
+		if currIndex > len(val) :
 			break
 	
 	return count
